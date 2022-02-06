9d945145504b0ede211c573226435e1ee3565bde
Tomas Fernandez <tfernandez@mulesoft.com>
2022-02-06 20:23:44 -0300

    Chore: enforcing .gitignore

--- build/js/packages/json-schema-validator-schema-validation-test/adapter-nodejs.js
+++
@@ -1,2 +0,0 @@
-require("/Users/tfernandez/projects/json-schema-validator/build/js/node_modules/kotlin-test-js-runner/kotlin-test-nodejs-runner.js")
-module.exports = require("/Users/tfernandez/projects/json-schema-validator/build/js/packages/json-schema-validator-schema-validation-test/kotlin/json-schema-validator-schema-validation-test.js")
